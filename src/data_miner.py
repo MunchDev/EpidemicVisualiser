8cad0fb2fc44829b2ff0764befa7a5d1d37f7300
MunchDev <54888003+MunchDev@users.noreply.github.com>
2020-05-03 17:43:06 +0800

    Add tally visualiser

--- src/data_miner.py
+++ src/data_miner.py
@@ -19,3 +19,33 @@ def get_country_data(country, days, date):
         entries.append(csv_processor.parse_data(helper.modify_date(date, -i))[country])
 
     return entries
+
+def country_tally_plot(country, date, timespan, scale, plot_type):
+    data = get_country_data(country, timespan, date)
+    confirmed = [x[0] for x in data]
+    deaths = [x[1] for x in data]
+    recovered = [x[2] for x in data]
+    active = [x[3] for x in data]
+
+    x_data = np.linspace(-(timespan-1), 0, num=timespan)
+    confirmed = np.array(confirmed)
+    deaths = np.array(deaths)
+    recovered = np.array(recovered)
+    active = np.array(active)
+
+    plt.yscale(scale)
+
+    if "c" in plot_type:
+        plt.plot(x_data, confirmed, '.b-', label="Confirmed")
+    if "r" in plot_type:
+        plt.plot(x_data, recovered, '.g-', label="Recovered")
+    if "d" in plot_type:
+        plt.plot(x_data, deaths, '.k-', label="Deaths")
+    if "a" in plot_type:
+        plt.plot(x_data, active, '.r-', label="Active")
+
+    plt.xlabel("Number of days since the latest report")
+    plt.ylabel("Number of cases")
+    plt.title("COVID-19 tally for " + country)
+    plt.legend(loc='best')
+    plt.show()
