2bff6407d319e184e7cd40c3213b253f97a341f0
MunchDev <54888003+MunchDev@users.noreply.github.com>
2020-05-03 19:59:40 +0800

    Add helper

--- src/helper.py
+++ src/helper.py
@@ -4,8 +4,6 @@ def modify_date(date, n):
     """
     Receive a date (format dd-mm-yyyy) and add a number of days into it.
     Use negative n for substraction.
-
-    Modified and adapted from: www.pressthered.com/adding_dates_and_times_in_python
     """
     date = datetime.strptime(date, "%d-%m-%Y")
     date += timedelta(days=n)
