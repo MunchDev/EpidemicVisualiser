56dfb0853ca517b82ab70847b50c41317452f37a
MunchDev <54888003+MunchDev@users.noreply.github.com>
2020-05-03 20:15:58 +0800

    Add function ‘is_valid_date’

--- src/helper.py
+++ src/helper.py
@@ -8,3 +8,17 @@ def modify_date(date, n):
     date = datetime.strptime(date, "%d-%m-%Y")
     date += timedelta(days=n)
     return date.strftime("%d-%m-%Y")
+
+def is_valid_date(date):
+    if type(date) != str:
+        return False
+    if len(date) != 10:
+        return False
+    c = date.split("-")
+    if len(c) != 3:
+        return False
+    if len(c[0]) != 2 or len(c[1]) != 2 or len(c[2]) != 4:
+        return False
+    if not (c[0].isdigit() and c[1].isdigit() and c[2].isdigit()):
+        return False
+    return True
