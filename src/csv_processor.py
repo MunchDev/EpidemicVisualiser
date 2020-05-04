a6628dec31759360f718bbe66d441e23bcb53c07
MunchDev <54888003+MunchDev@users.noreply.github.com>
2020-05-04 21:28:41 +0800

    Explicitly import require symbols

--- src/csv_processor.py
+++ src/csv_processor.py
@@ -1,6 +1,6 @@
 from helper import print_error as stderr
 from urllib.request import urlopen
-from os import path
+from os.path import isfile
 from json import load, dumps
 from csv import reader
 from io import StringIO
@@ -10,15 +10,15 @@ def fetch_data(date):
     url += "/csse_covid_19_data/csse_covid_19_daily_reports/"
     url += date[3:5] + "-" + date[:2] + "-" + date[6:] + ".csv"
     try:
-        return list(csv.reader(StringIO("\n".join(list(map(lambda a: str(a, "utf-8")[:-1], list(urlopen(url))))))))
+        return list(reader(StringIO("\n".join(list(map(lambda a: str(a, "utf-8")[:-1], list(urlopen(url))))))))
     except:
         stderr("No dataset found for {}.".format(date))
         return -1 
 def parse_data(date):
     filename = "../cache/" + "".join(date.split("-")) + ".json"
-    if path.isfile(filename):
+    if isfile(filename):
         wite open(filename) as f:
-            return json.load(f)	
+            return load(f)	
     data = fetch_data(date).pop(0)
     data_set = dict()
     for entry in data:
@@ -31,5 +31,5 @@ def parse_data(date):
         else:
             data_set[country_name] = [int(entry[7]), int(entry[8]), int(entry[9]), int(entry[10])]	
     with open("../cache/" + "".join(date.split("-")) + ".json", "w") as f:
-        f.write(json.dumps(data_set))
+        f.write(dumps(data_set))
     return data_set
