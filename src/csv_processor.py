ff9ad01bf04300c5770dd905cb63872c4f2623ee
MunchDev <54888003+MunchDev@users.noreply.github.com>
2020-05-04 21:20:49 +0800

    Code optimisation

--- src/csv_processor.py
+++ src/csv_processor.py
@@ -1,81 +1,35 @@
+from helper import print_error as stderr
 from urllib.request import urlopen
 from os import path
-import json
-import csv
+from json import load, dumps
+from csv import reader
 from io import StringIO
 
 def fetch_data(date):
-    """
-    Fetch data for the date (in dd-mm-yyyy format) from a Github repository.
-    Index:			            0123456789
-    """
-
-    #URL builder
     url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master"
     url += "/csse_covid_19_data/csse_covid_19_daily_reports/"
     url += date[3:5] + "-" + date[:2] + "-" + date[6:] + ".csv"
-
-    #Download CSV
     try:
-	    downloaded_data = list(urlopen(url))
+        return list(csv.reader(StringIO("\n".join(list(map(lambda a: str(a, "utf-8")[:-1], list(urlopen(url))))))))
     except:
-        print("[Fatal] No dataset found for {}".format(date))
-        exit()
-
-    downloaded_data = list(map(lambda a: str(a, "utf-8")[:-1], downloaded_data))
-
-    return list(csv.reader(StringIO("\n".join(downloaded_data))))
-
+        stderr("No dataset found for {}.".format(date))
+        return -1 
 def parse_data(date):
-    """
-    Return a dictionary of country and their respective Covid-19 statistics.
-    Return format: Dictionary(country_name, List(confirmed, deaths, recovered, active))
-    
-    For countries with multiple entries (i.e US, Canada, Australia), their state
-    and provincial entries will be merged under one single entry.
-    """
-
-    #Check if data set already exists
-    filename = "".join(date.split("-")) + ".json"
-    if path.isfile("../cache/" + filename):
-        f = open("../cache/" + filename)
-        data_set = json.load(f)	
-        f.close()
-        return data_set
-    
-    #Retrive data
-    data = fetch_data(date)
-    data.pop(0)		#Ignore first line
-	
-    # CSV format:
-    # FIPS,Admin2,Province_State,Country_Region,Last_Update,Lat,Long_,Confirmed,Deaths,Recovered,Active,Combined_Key
-    # 
-    # Use only: Country_Region(3), Confirmed(7), Deaths(8), Recovered(9), Active(10)
-    #
-    # For countries with multiple entries, merge them.
-
-    #Process
+    filename = "../cache/" + "".join(date.split("-")) + ".json"
+    if path.isfile(filename):
+        wite open(filename) as f:
+            return json.load(f)	
+    data = fetch_data(date).pop(0)
     data_set = dict()
     for entry in data:
         country_name = entry[3]
-        confirmed = int(entry[7])
-        deaths = int(entry[8])
-        recovered = int(entry[9])
-        active = int(entry[10])
-
         if country_name in data_set.keys():
-            data_set[country_name][0] += confirmed
-            data_set[country_name][1] += deaths
-            data_set[country_name][2] += recovered
-            data_set[country_name][3] += active
+            data_set[country_name][0] += int(entry[7])
+            data_set[country_name][1] += int(entry[8])
+            data_set[country_name][2] += int(entry[9])
+            data_set[country_name][3] += int(entry[10])
         else:
-            data_set[country_name] = [confirmed, deaths, recovered, active]
-	
-    #Save processed data for future use
-    filename = "".join(date.split("-")) + ".json"
-    f = open("../cache/" + filename, "w")
-    f.write(json.dumps(data_set))
-    f.close()
-
-    #Return data set
+            data_set[country_name] = [int(entry[7]), int(entry[8]), int(entry[9]), int(entry[10])]	
+    with open("../cache/" + "".join(date.split("-")) + ".json", "w") as f:
+        f.write(json.dumps(data_set))
     return data_set
