319568b6882a0bf5ef12ba312d319999059264e6
MunchDev <binhnt.mdev@gmail.com>
2020-05-05 03:22:57 +0000

    Fix test naming convention

--- src/test_a.py
+++ src/test_a.py
@@ -1,4 +1,5 @@
 from data_miner import plot_tally
+import pytest
 
 d_country = ["Vietnam", "Singapore", "US", "Malaysia"]
 d_date = "03-05-2020"
@@ -6,8 +7,8 @@ d_timespan = 40
 d_scale = ["linear", "log", "log", "linear"]
 d_plot_type = "cdra"
 
-def functional_test_A():
+def test_A():
     assert plot_tally(d_country, d_date, d_timespan, scale=d_scale, plot_type=d_plot_type) == 0
 
-def functional_test_B():
+def test_B():
     assert plot_tally(d_country, d_date, d_timespan, scale=d_scale, plot_type=d_plot_type, transpose=True) == 0
